127bf949b631e78be2673fa5839c4c4a995b92ae
TAM <levantamdhcn@gmail.com>
2021-11-25 10:19:55 +0700

    update button logout

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,6 +3,19 @@ import { useDispatch,useSelector } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { userActions } from '../stores/index'
 
+const buttonStyles = {
+    cursor: "pointer",
+    padding: ".5rem 1rem",
+    fontSize: "1.25rem",
+    borderRadius: ".3rem",
+    color: "#fff",
+    backgroundColor: "#0d6efd",
+    borderColor: "#0d6efd",
+    display: "inline-block",
+    fontWeight: "400",
+    lineHeight: '1.5',
+}
+
 function HomePage() {
     const dispatch = useDispatch()
     const { logout } = bindActionCreators(userActions, dispatch)
@@ -14,7 +27,7 @@ function HomePage() {
     return (
         <div>
             <h1>Welcome {userInfoState.email}</h1>
-            <button onClick={handleLogout}>
+            <button onClick={handleLogout} style={buttonStyles}>
                 Logout
             </button>
         </div>
